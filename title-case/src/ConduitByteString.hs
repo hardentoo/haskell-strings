03842c9d87d4628441b6727188d678c7c9a7a041
Steven Shaw <steven@steshaw.org>
2017-06-12 13:05:03 +1000

    Formatting

--- title-case/src/ConduitByteString.hs
+++ title-case/src/ConduitByteString.hs
@@ -3,10 +3,10 @@
 module Main where
 
 import Conduit
-import Data.Conduit.List (mapAccum, chunksOf)
 import Control.Monad
-import GHC.Word (Word8)
 import Data.ByteString.Lazy.Internal (defaultChunkSize)
+import Data.Conduit.List (chunksOf, mapAccum)
+import GHC.Word (Word8)
 
 import qualified Data.ByteString as B
 
@@ -19,44 +19,41 @@ space = chr ' '
 lf :: Word8
 lf = chr '\n'
 
-isSpace ∷ Word8 → Bool
+isSpace :: Word8 -> Bool
 isSpace w = w == space || w == lf
 
 toUpper :: Word8 -> Word8
-toUpper x = if x >= chr 'a' && x <= chr 'z' then x - space else x
+toUpper x =
+  if x >= chr 'a' && x <= chr 'z'
+    then x - space
+    else x
 
-op ∷ Bool → Word8 → (Bool, Word8)
-op flag c = if isSpace c then (True, c) else (False, d)
-    where d = if flag then toUpper c else c
-
--- fd  ∷ Word8 → () → ((), [Word8])
-op2 ∷ Word8 → Bool → (Bool, Word8)
-op2 = flip op
+op :: Bool -> Word8 -> (Bool, Word8)
+op flag c =
+  if isSpace c
+    then (True, c)
+    else (False, d)
+  where
+    d =
+      if flag
+        then toUpper c
+        else c
 
 -- mapAccum  :: (a -> s -> (s, b)) -> s -> ConduitM a b m s
 -- mapAccumL :: (acc -> Word8 -> (acc, Word8)) -> acc -> ByteString -> (acc, ByteString)
-
-data Hole = Hole
-
 chunkify :: (Monad m) => ConduitM Word8 [Word8] m ()
 chunkify = chunksOf defaultChunkSize
+
 {-
   some <- takeC defaultChunkSize
   yield some
   chunkify
 -}
-
--- capitaliseC :: ConduitM Word8 Word8 (ResourceT IO) Bool
 capitaliseC :: ConduitM Word8 Word8 (ResourceT IO) ()
-capitaliseC = void $ mapAccum op2 True
+capitaliseC = void $ mapAccum (flip op) True
 
-main ∷ IO ()
-main = runConduitRes $ sourceFile "input.txt"
---  .| decodeUtf8C
---  .| mapAccum op2 True
-  .| concatC
-  .| capitaliseC
-  .| chunkify
-  .| mapC B.pack
---  .| encodeUtf8C
-  .| stdoutC
+main :: IO ()
+main =
+  runConduitRes $
+  sourceFile "input.txt" .| concatC .| capitaliseC .| chunkify .| mapC B.pack .|
+  stdoutC
