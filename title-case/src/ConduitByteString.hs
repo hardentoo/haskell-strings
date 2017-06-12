877a7356b649d9844be7d787f6faef978419c662
Steven Shaw <steven@steshaw.org>
2017-06-12 18:31:12 +1000

    Tweaks

--- title-case/src/ConduitByteString.hs
+++ title-case/src/ConduitByteString.hs
@@ -4,9 +4,11 @@ module Main where
 
 import Conduit
 import Control.Monad
+import Criterion.Main
 import Data.ByteString.Lazy.Internal (defaultChunkSize)
 import Data.Conduit.List (chunksOf, mapAccum)
 import GHC.Word (Word8)
+import System.Environment
 
 import qualified Data.ByteString as B
 
@@ -28,30 +30,47 @@ toUpper x =
     then x - space
     else x
 
-op :: Bool -> Word8 -> (Bool, Word8)
-op flag c =
-  if isSpace c
-    then (True, c)
-    else (False, d)
+capitalise :: Bool -> Word8 -> (Bool, Word8)
+capitalise wasSpace c = (isSpace c, c')
   where
-    d =
-      if flag
+    c' =
+      if wasSpace
         then toUpper c
         else c
 
-chunkify :: (Monad m) => ConduitM Word8 [Word8] m ()
-chunkify = chunksOf defaultChunkSize
-{-
-  some <- takeC defaultChunkSize
-  yield some
-  chunkify
--}
+chunkSize :: Int
+chunkSize =
+  if useSmallChunk
+    then 8 * 1024
+    else defaultChunkSize
+  where
+    useSmallChunk = True
 
+-- Old try at implementing "chunkify"
+-- chunkify = do
+--  some <- takeC defaultChunkSize
+--  yield some
+--  chunkify
 capitaliseC :: ConduitM Word8 Word8 (ResourceT IO) ()
-capitaliseC = void $ mapAccum (flip op) True
+capitaliseC = void $ mapAccum (flip capitalise) True
+
+doMain :: Int -> IO ()
+doMain chunkSize =
+  runConduitRes $
+  sourceFile "input.txt" .| concatC .| capitaliseC .| chunksOf chunkSize .|
+  mapC B.pack .|
+  stdoutC
+
+benchmark =
+  defaultMain
+    [ bgroup
+        "conduit-byte-string"
+        (map (\n -> bench (show n) $ whnfIO (doMain n)) [256, 512])
+    ]
 
 main :: IO ()
-main =
-  runConduitRes $
-  sourceFile "input.txt" .| concatC .| capitaliseC .| chunkify .| mapC B.pack .|
-  stdoutC
+main = do
+  args <- getArgs
+  case args of
+    ["run", d] -> doMain (read d)
+    _ -> benchmark
--- title-case/src/ConduitText.hs
+++ title-case/src/ConduitText.hs
@@ -3,23 +3,31 @@
 module Main where
 
 import Conduit
+import Control.Monad (void)
 import Data.Char (toUpper)
+import Data.Conduit.List
+import qualified Data.Text as T
 
-{-
-isSpace ∷ Word8 → Bool
-isSpace w = w == _space || w == _nbsp || w <= _cr && w >= _tab
+isSpace :: Char -> Bool
+isSpace c = c == ' ' || c == '\n'
 
-toUpper ∷ Word8 → Word8
-toUpper w = if isAsciiLower w then w - _space else w
+capitalise :: Bool -> Char -> (Bool, Char)
+capitalise wasSpace c = (isSpaceC, c1)
+  where
+    isSpaceC = isSpace c
+    c1 =
+      if wasSpace
+        then toUpper c
+        else c
 
-op ∷ Bool → Word8 → (Bool, Word8)
-op flag c = if isSpace c then (True,c) else (False,d)
-    where d = if flag then toUpper c else c
--}
+capitaliseC :: ConduitM Char Char (ResourceT IO) ()
+capitaliseC = void (mapAccum (flip capitalise) True)
 
-main ∷ IO ()
-main = runConduitRes $ sourceFile "input.txt"
-  .| decodeUtf8C
-  .| omapCE toUpper -- mapC (T.map toUpper)
-  .| encodeUtf8C
-  .| stdoutC
+main :: IO ()
+main =
+  runConduitRes $
+  sourceFile "input.txt" .| decodeUtf8C .| concatC .| capitaliseC .|
+  chunksOf 256 .|
+  mapC T.pack .|
+  encodeUtf8C .|
+  stdoutC
